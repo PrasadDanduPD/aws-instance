4cff23727f5ffc6968e3e058240f2453dfadcf24
PrasadDanduPD <prasadvarma.2294@gmail.com>
2024-07-23 12:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "one" {
+  count = 5
   ami           = "ami-068e0f1a600cd311c" 
   instance_type = "t2.micro"  
   tags = {
